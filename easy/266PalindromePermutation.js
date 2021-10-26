7c87851342117b51450c43576672be36e04e86a3
iparkgood <zuzu8723@gmail.com>
2021-10-25 21:34:45 -0700

    266. Palindrome Permutation - solution 2 (slower)

--- easy/266PalindromePermutation.js
+++ easy/266PalindromePermutation.js
@@ -2,20 +2,12 @@ const canPermutePalindrome = (s) => {
   let hashMap = {};
 
   for (let i = 0; i < s.length; i++) {
-    hashMap[s[i]] = (hashMap[s[i]] || 0) + 1;
+    if (hashMap.hasOwnProperty(s[i])) {
+      delete hashMap[s[i]];
+    } else {
+      hashMap[s[i]] = 1;
+    }
   }
 
-  if (s.length % 2 === 0) {
-    for (const prop in hashMap) {
-      if (hashMap[prop] % 2 !== 0) return false;
-    }
-  } else {
-    let oddCount = 0;
-    for (const prop in hashMap) {
-      if (hashMap[prop] % 2 !== 0) oddCount++;
-    }
-    if (oddCount !== 1) return false;
-  }
-
-  return true;
+  return Object.values(hashMap) <= 1 ? true : false;
 };
