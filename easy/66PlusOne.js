0fc5f33e72181c8fa0e457b88fabbde3fe5366b5
iparkgood <zuzu8723@gmail.com>
2021-10-27 16:27:05 -0700

    66. Plus One - refactored

--- easy/66PlusOne.js
+++ easy/66PlusOne.js
@@ -1,22 +1,9 @@
 const plusOne = (digits) => {
-  digits = digits.map((d) => +d);
+  const str = digits.join("");
+  const numbers = BigInt(str);
+  const increment = numbers + 1n;
 
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] < 9) {
-      digits[i] += 1;
-      break;
-    } else {
-      digits[i] += 1;
-      const first = +digits[i].toString().slice(0, 1);
-      digits[i] = 0;
-
-      if (i === 0) {
-        digits.unshift(first);
-      }
-    }
-  }
-
-  return digits;
+  return increment.toString().split("");
 };
 
 console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
