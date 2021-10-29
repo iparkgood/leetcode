a1546ee72491c28199c6ed89fc3f8251606b34a4
iparkgood <zuzu8723@gmail.com>
2021-10-29 14:08:37 -0700

    125. Valid Palindrome - solution 2

--- easy/125ValidPalindrome.js
+++ easy/125ValidPalindrome.js
@@ -1,12 +1,11 @@
 var isPalindrome = function (s) {
-  let lowerCase = s.toLowerCase();
-  let letters = lowerCase.replace(/[^a-z0-9]/g, "");
-
-  let reverse = "";
-
-  for (let i = letters.length - 1; i >= 0; i--) {
-    reverse += letters[i];
-  }
-
-  return letters === reverse;
+  return (
+    s.toLowerCase().replace(/[^a-z0-9]/g, "") ===
+    s
+      .toLowerCase()
+      .replace(/[^a-z0-9]/g, "")
+      .split("")
+      .reverse()
+      .join("")
+  );
 };
