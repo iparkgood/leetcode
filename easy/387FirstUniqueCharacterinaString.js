0ef70972fea6ed07fe69d473f855953ff352ba8d
iparkgood <zuzu8723@gmail.com>
2021-11-02 11:23:15 -0700

    387. First Unique Character in a String

---
+++ easy/387FirstUniqueCharacterinaString.js
@@ -0,0 +1,13 @@
+const firstUniqChar = (s) => {
+  let hashMap = {};
+
+  for (let i = 0; i < s.length; i++) {
+    hashMap[s[i]] = (hashMap[s[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (hashMap[s[i]] === 1) return i;
+  }
+
+  return -1;
+};
