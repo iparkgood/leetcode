46809600aa8703e05d4802d1c9091a5e1a0bc3e5
iparkgood <zuzu8723@gmail.com>
2021-11-07 13:23:56 -0800

    997. Find the Town Judge - solution 2 using one array

--- easy/997FindtheTownJudge.js
+++ easy/997FindtheTownJudge.js
@@ -1,32 +1,16 @@
 const findJudge = (n, trust) => {
-  //create a hash table
-  //find a number that the hash table doesn't contain as a key - loop using n 
-  //check if all other values have the number as their value
-    //yes - return the number as a judge
-    //no - return -1
+  if (trust.length < n - 1) return -1;
 
-  if (!trust.length && n >= 2) return -1;
-
-  let hashMap = {};
-  let judge = 0;
+  let trustCounts = new Array(n + 1).fill(0);
 
   trust.forEach(([a, b]) => {
-    if (!hashMap[a]) {
-      hashMap[a] = [b];
-    } else {
-      hashMap[a].push(b);
-    }
+    trustCounts[a]--;
+    trustCounts[b]++;
   })
 
-  for (let i = 1; i <= n; i++) {
-    if (!hashMap[i]) judge = i;
+  for (let i = 1; i < trustCounts.length; i++) {
+    if (trustCounts[i] === n - 1) return i;
   }
 
-  if (judge === 0) return -1;
-
-  for (const key in hashMap) {
-    if (!hashMap[key].includes(judge)) return -1;
-  }
-
-  return judge;
+  return -1;
 }
