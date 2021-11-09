731796018f318a2ed78216d85e05423f5085c3d8
iparkgood <zuzu8723@gmail.com>
2021-11-09 12:28:11 -0800

    404. Sum of Left Leaves

--- easy/404SumofLeftLeaves.js
+++ easy/404SumofLeftLeaves.js
@@ -1,9 +1,24 @@
-const sumOfLeftLeaves = (root, isLeft = false) => {
-  if (!root) return 0;
-  if (!root.left && !root.right) return isLeft ? root.val : 0;
+const sumOfLeftLeaves = (root) => {
+  let sum = 0;
 
-  return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right);
+  const traverse = (node) => {
+    if (!node) return 0;
+
+    if (node.left) {
+      traverse(node.left);
+      if (!node.left.left && !node.left.right) {
+        sum += node.left.val;
+      }
+    }
+    if (node.right) {
+      traverse(node.right);
+    }
+  }
+  if (root) traverse(root);
+
+  return sum;
 };
+
 
 /**
  * Input: root = [3,9,20,null,null,15,7]
