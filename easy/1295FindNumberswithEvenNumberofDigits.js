0ac8b237e227aeab601affee7b89d872f147740a
iparkgood <zuzu8723@gmail.com>
2021-11-29 16:38:27 -0800

    1295. Find Numbers with Even Number of Digits - refactored

--- easy/1295FindNumberswithEvenNumberofDigits.js
+++ easy/1295FindNumberswithEvenNumberofDigits.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 const findNumbers = (nums) => {
-  let count = 0;
-
-  for (const num of nums) {
-    if (num.toString().length % 2 === 0) count++;
-  }
-
-  return count;
+  return nums.filter(num => num.toString().length % 2 === 0).length;
 };
