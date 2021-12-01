f91e30672bfaebbb74113f27df7f68dcf3188106
iparkgood <zuzu8723@gmail.com>
2021-12-01 12:42:41 -0800

    1064. Fixed Point - binary search

--- easy/1064FixedPoint.js
+++ easy/1064FixedPoint.js
@@ -3,8 +3,16 @@
  * @return {number}
  */
 const fixedPoint = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === i) return i;
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === mid && arr[mid - 1] !== mid - 1) return mid;
+    else if (arr[mid] < mid) left = mid + 1;
+    else right = mid;
   }
+
   return -1;
 };
