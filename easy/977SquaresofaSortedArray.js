deffffbe09c24fbb0534dbb4569754879649cd65
iparkgood <zuzu8723@gmail.com>
2021-12-05 23:45:38 -0800

    977. Squares of a Sorted Array - two pointers

--- easy/977SquaresofaSortedArray.js
+++ easy/977SquaresofaSortedArray.js
@@ -3,5 +3,21 @@
  * @return {number[]}
  */
 const sortedSquares = function (nums) {
-  return nums.map(num => num ** 2).sort((a, b) => a - b);
+  let res = [];
+  let left = 0;
+  let right = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let square;
+    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      square = nums[right];
+      right--;
+    } else {
+      square = nums[left];
+      left++;
+    }
+    res[i] = square * square;
+  }
+
+  return res;
 };
