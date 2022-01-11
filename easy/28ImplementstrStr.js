5dabdc3ed77a4ed4fac9688e9c148533799851c5
iparkgood <zuzu8723@gmail.com>
2022-01-11 11:30:08 -0800

    28. Implement strStr() - refactored again

--- easy/28ImplementstrStr.js
+++ easy/28ImplementstrStr.js
@@ -6,15 +6,9 @@
 const strStr = (haystack, needle) => {
   if (!needle) return 0;
 
-  let j = 0;
-
   for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] === needle[j]) {
-      if (needle === haystack.slice(i, i + needle.length)) {
-        return i;
-      } else {
-        j = 0;
-      }
+    if (needle === haystack.slice(i, i + needle.length)) {
+      return i;
     }
   }
 
